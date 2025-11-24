eacd52ac8f3ddf83fdafd7259f4ce9c3889bf9fc
Sanjana Desai <sanjanadesai1383@gmail.com>
2025-11-24 07:49:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "course-compass-xplhv",
+  "appId": "1:979907008515:web:73935d364b8140a4609636",
+  "apiKey": "AIzaSyBkYy7lirHzYOaGBaM8GeNaRGvghIGkxSo",
+  "authDomain": "course-compass-xplhv.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979907008515"
+};
