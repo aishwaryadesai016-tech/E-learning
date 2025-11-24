a30336e9eb83d25a39911d589216c81ccfad5c36
Sanjana Desai <sanjanadesai1383@gmail.com>
2025-11-24 07:31:36 +0000

    fix the sidebar as make it, fixed even also keep the fixed position of t

--- src/app/courses/layout.tsx
+++ src/app/courses/layout.tsx
@@ -1,3 +1,4 @@
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
@@ -12,7 +13,7 @@ export default function CoursesLayout({
 }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-card md:block">
+      <div className="hidden border-r bg-card md:fixed md:inset-y-0 md:left-0 md:z-10 md:block md:w-[220px] lg:w-[280px]">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Logo />
@@ -59,8 +60,8 @@ export default function CoursesLayout({
           </div>
         </div>
       </div>
-      <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6">
+      <div className="flex flex-col md:pl-[220px] lg:pl-[280px]">
+        <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-10">
           <Sheet>
             <SheetTrigger asChild>
               <Button
@@ -130,7 +131,7 @@ export default function CoursesLayout({
             <span className="sr-only">Toggle notifications</span>
           </Button>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background/50">
+        <main className="flex-1 p-4 lg:p-6 bg-background/50">
           {children}
         </main>
       </div>
